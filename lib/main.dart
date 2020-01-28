9fd7875dee0756e706455b41dc5aee5d1259e338
nischal tiwari <nikks068@gmail.com>
2020-01-28 16:53:53 +0545

    Added Dicee App to github

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MainPage());
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Dicee",
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text("Dicee"),
+        ),
+        body: DicePage(),
+      ),
+    );
+  }
+}
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+
+class _DicePageState extends State<DicePage> {
+  int firstDiceNumber = 1;
+  int secondDiceNumber = 1;
+
+  void randomDice(){
+    setState(() {
+      firstDiceNumber =Random().nextInt(6) +1;
+      secondDiceNumber=Random().nextInt(6) +1;
+
+    });
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(flex:2,child:
+          FlatButton(
+              onPressed: (){
+                randomDice();
+              },
+              padding: EdgeInsets.only(left: 10),
+              child: Image.asset("images/dice$firstDiceNumber.png"))),
+          SizedBox(
+            width: 20,
+          ),
+          Expanded(flex:2,child:
+          FlatButton(
+              onPressed: (){
+                randomDice();
+
+              },
+              padding: EdgeInsets.only(right: 10),
+              child: Image.asset("images/dice$secondDiceNumber.png"))),
+        ],
+      ),
+    );
+  }
+}
+
+
